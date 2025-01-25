f5eb1c2053bd76fc6b165c90b3af7a10f8a6a06c
Angad Dubey <angaddubey10@gmail.com>
2025-01-25 11:20:27 +0530

    Ticket Issue - Controller: Added In

--- src/parkinglot/controllers/TicketController.java
+++ src/parkinglot/controllers/TicketController.java
@@ -2,12 +2,32 @@ package parkinglot.controllers;
 
 import parkinglot.dtos.IssueTicketRequestDto;
 import parkinglot.dtos.IssueTicketResponseDto;
+import parkinglot.dtos.ResponseStatus;
+import parkinglot.exceptions.GateNotFoundException;
+import parkinglot.exceptions.ParkingLotNotFoundException;
+import parkinglot.models.Ticket;
+import parkinglot.services.TicketService;
 
 public class TicketController {
+    private TicketService ticketService;
 
-    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto){
-//        authService.authenticate(token);
-//        issueTicket()
-        return null;
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto request){
+        IssueTicketResponseDto issueTicketResponseDto = new IssueTicketResponseDto();
+        Ticket ticket;
+        try{
+            ticket = ticketService.issueTicket(
+                    request.getVehicleType(),
+                    request.getVehicleNumber(),
+                    request.getVehicleOwnerName(),
+                    request.getGateId()
+            );
+        } catch (Exception e) {
+            issueTicketResponseDto.setResponseStatus(ResponseStatus.ERROR);
+            issueTicketResponseDto.setErrorMessage(e.getMessage());
+            return issueTicketResponseDto;
+        }
+        issueTicketResponseDto.setResponseStatus(ResponseStatus.SUCCESS);
+        issueTicketResponseDto.setTicket(ticket);
+        return issueTicketResponseDto;
     }
 }
