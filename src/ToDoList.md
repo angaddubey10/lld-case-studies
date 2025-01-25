ed08319e553dde30d75571c887de4a00d6494a36
Angad Dubey <angaddubey10@gmail.com>
2025-01-25 11:12:03 +0530

    In-Memory Database Creation

--- src/ToDoList.md
+++ src/ToDoList.md
@@ -1,3 +1,7 @@
 # To-Do List
 1. Learn about Stateful and Stateless 
    2. Having states cause problem. 
+
+# In Memory DB
+1. Use a List
+2. Use a Map - Both works fine. Map is better
--- src/parkinglot/repositories/ParkingLotRepository.java
+++ src/parkinglot/repositories/ParkingLotRepository.java
@@ -3,10 +3,18 @@ package parkinglot.repositories;
 import parkinglot.models.Gate;
 import parkinglot.models.ParkingLot;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 public class ParkingLotRepository {
+    private Map<Long, ParkingLot> parkingLots = new TreeMap<>();
     public Optional<ParkingLot> getParkingLotForGate(Gate gate){
+        for(ParkingLot parkingLot: parkingLots.values()){
+            if(parkingLot.getGates().contains(gate)){
+                return Optional.of(parkingLot);
+            }
+        }
         return Optional.empty();
     }
 }
